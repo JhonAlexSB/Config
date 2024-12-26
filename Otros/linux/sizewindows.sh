3334c79292f8f7f49a10d669082f92ade5f45a18
JhonAlexSB <jhonalexsalazar04@gmail.com>
2024-12-26 09:35:45 -0500

    add linux notes files

--- Otros/linux/sizewindows.sh
+++ Otros/linux/sizewindows.sh
@@ -6,7 +6,16 @@
 
 #https://unix.stackexchange.com/questions/227876/how-to-set-custom-resolution-using-xrandr-when-the-resolution-is-not-available-i
 #gtf 1900 1400 30
+#gtf 1850 1090 30
 
 xrandr --newmode "1904x950_30.00"  68.70  1904 1944 2136 2368  950 951 954 967
 xrandr --addmode Virtual1 "1904x950_30.00"
 xrandr -s 1904x950_30.00
+#gtf 1080 720 30
+
+#xrandr --newmode "1848x1090_30.00"  77.98  1848 1912 2096 2344  1090 1091 1094 1109
+#xrandr --addmode HDMI-1 "1848x1090_30.00"
+
+#xrandr --newmode "1080x720_30.00"  28.32  1080 1080 1184 1288  720 721 724 733
+#xrandr --addmode HDMI-1 "1080x720_30.00"
+
